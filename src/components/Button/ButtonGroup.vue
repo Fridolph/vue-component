51f4469fcd721ee3380365364b8058cc33829810
fridolph <249121486@qq.com>
2019-04-09 16:11:26 +0800

    button 组件（icon 和 button-group） g-button-group 检测

--- src/components/Button/ButtonGroup.vue
+++ src/components/Button/ButtonGroup.vue
@@ -6,7 +6,16 @@
 
 <script>
 export default {
+  name: 'g-btn-group',
+  mounted() {
+    for (let node of this.$el.children) {
+      let name = node.nodeName.toLowerCase()
+      if (name !== 'button') {
+        console.warn(`g-button-group 的子元素应该全是 g-button , 但现在是 ${name}`)
 
+      }
+    }
+  }
 }
 </script>
 
