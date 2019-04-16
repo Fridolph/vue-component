c0a56711e5f7bdba146c237eb5eb77b0a0aaee0e
fridolph <249121486@qq.com>
2019-04-16 15:32:25 +0800

    添加响应式支持

--- src/components/Grid/Col.vue
+++ src/components/Grid/Col.vue
@@ -9,15 +9,31 @@
 </template>
 
 <script>
+let validator = value => {
+  let keys = Object.keys(value)
+  let valid = true
+  keys.forEach(key => {
+    if (!['span', 'offset'].includes(key)) valid = false
+  })
+  return valid
+}
+
 export default {
   name: 'g-col',
   props: {
     span: {
       type: [Number, String],
+      default: 24
     },
     offset: {
       type: [Number, String],
     },
+    xs: { type: Object, validator },
+    sm: { type: Object, validator },
+    md: { type: Object, validator },
+    lg: { type: Object, validator },
+    xl: { type: Object, validator },
+    xxl: { type: Object, validator },
   },
   data() {
     return {
@@ -26,8 +42,17 @@ export default {
   },
   computed: {
     colCls() {
-      let { span, offset } = this
-      return [span && `g-col-span-${span}`, offset && `g-col-offset-${offset}`]
+      let { span, offset, xs, sm, md, lg, xl, xxl } = this
+      return [
+        span && `g-col-span-${span}`,
+        offset && `g-col-offset-${offset}`,
+        ...(xs ? [`g-col-xs-${xs.span}`] : []),
+        ...(sm ? [`g-col-sm-${sm.span}`] : []),
+        ...(md ? [`g-col-md-${md.span}`] : []),
+        ...(lg ? [`g-col-lg-${lg.span}`] : []),
+        ...(xl ? [`g-col-xl-${xl.span}`] : []),
+        ...(xxl ? [`g-col-xxl-${xxl.span}`] : []),
+      ]
     },
     colStyle() {
       let { gutter } = this
@@ -49,11 +74,37 @@ $gridColumns = 24
   // display inline-flex
   flex 0 0 1
 
-for column in 1 .. $gridColumns
-  .g-col-span-{column}
-    width: (column / $gridColumns) * 100%
+  for column in 1 .. $gridColumns
+    &-span-{column}
+      width: (column / $gridColumns) * 100%
 
-for offset in 1 .. $gridColumns
-  .g-col-offset-{offset}
-    margin-left: (offset / $gridColumns) * 100%
+  for offset in 1 .. $gridColumns
+    &-offset-{offset}
+      margin-left: (offset / $gridColumns) * 100%
+
+  @media (max-width: 576px)
+    for $n in 1 .. $gridColumns
+      &-xs-{$n}
+        width ($n / 24) * 100%
+  @media (min-width: 577px) and (max-width: 768px)
+    for $n in 1 .. $gridColumns
+      &-sm-{$n}
+        width ($n / 24) * 100%
+  @media (min-width: 769px) and (max-width: 991px)
+    for $n in 1 .. $gridColumns
+      &-md-{$n}
+        width ($n / 24) * 100%
+  @media (min-width: 992px) and (max-width: 1199px)
+    for $n in 1 .. $gridColumns
+      &-lg-{$n}
+        width ($n / 24) * 100%
+  @media (min-width: 1200px) and (max-width: 1599px)
+    for $n in 1 .. $gridColumns
+      &-xl-{$n}
+        width ($n / 24) * 100%
+  @media (min-width: 1600px)
+    for $n in 1 .. $gridColumns
+      &-xxl-{$n}
+        width ($n / 24) * 100%
+
 </style>
--- src/views/grid.vue
+++ src/views/grid.vue
@@ -2,37 +2,142 @@
   <div>
     <h3>Grid - 栅格系统</h3>
     <g-row>
-      <g-col span="12"><div>1</div></g-col>
-      <g-col span="12"><div>2</div></g-col>
+      <g-col span="12">
+        <div>1</div>
+      </g-col>
+      <g-col span="12">
+        <div>2</div>
+      </g-col>
     </g-row>
     <br>
     <g-row>
-      <g-col span="4"><div>1</div></g-col>
-      <g-col span="16"><div>2</div></g-col>
-      <g-col span="4"><div>3</div></g-col>
+      <g-col span="4">
+        <div>1</div>
+      </g-col>
+      <g-col span="16">
+        <div>2</div>
+      </g-col>
+      <g-col span="4">
+        <div>3</div>
+      </g-col>
     </g-row>
     <br>
     <g-row>
-      <g-col span="1"><div>1</div></g-col>
-      <g-col span="8" offset="3"><div>2</div></g-col>
-      <g-col span="3" offset="2"><div>3</div></g-col>
-      <g-col span="3" offset="4"><div>4</div></g-col>
+      <g-col span="1">
+        <div>1</div>
+      </g-col>
+      <g-col
+        span="8"
+        offset="3"
+      >
+        <div>2</div>
+      </g-col>
+      <g-col
+        span="3"
+        offset="2"
+      >
+        <div>3</div>
+      </g-col>
+      <g-col
+        span="3"
+        offset="4"
+      >
+        <div>4</div>
+      </g-col>
     </g-row>
     <br>
     <g-row :gutter="16">
-      <g-col span="6"><div>1</div></g-col>
-      <g-col span="6"><div>2</div></g-col>
-      <g-col span="6"><div>3</div></g-col>
-      <g-col span="6"><div>4</div></g-col>
+      <g-col span="6">
+        <div>1</div>
+      </g-col>
+      <g-col span="6">
+        <div>2</div>
+      </g-col>
+      <g-col span="6">
+        <div>3</div>
+      </g-col>
+      <g-col span="6">
+        <div>4</div>
+      </g-col>
     </g-row>
     <br>
     <g-row :gutter="32">
-      <g-col span="4"><div>1</div></g-col>
-      <g-col span="4"><div>2</div></g-col>
-      <g-col span="4"><div>3</div></g-col>
-      <g-col span="4"><div>4</div></g-col>
-      <g-col span="4"><div>5</div></g-col>
-      <g-col span="4"><div>6</div></g-col>
+      <g-col span="4">
+        <div>1</div>
+      </g-col>
+      <g-col span="4">
+        <div>2</div>
+      </g-col>
+      <g-col span="4">
+        <div>3</div>
+      </g-col>
+      <g-col span="4">
+        <div>4</div>
+      </g-col>
+      <g-col span="4">
+        <div>5</div>
+      </g-col>
+      <g-col span="4">
+        <div>6</div>
+      </g-col>
+    </g-row>
+    <br>
+    <h4>响应式</h4>
+    <g-row>
+      <g-col
+        :xs="{span: 24}"
+        :sm="{span: 16}"
+        :md="{span: 12}"
+        :lg="{span:16}"
+        :xl="{span: 7}"
+        :xxl="{span: 6}"
+      >
+        <div>1</div>
+      </g-col>
+      <g-col
+        :xs="{span: 24}"
+        :sm="{span: 8}"
+        :md="{span: 12}"
+        :lg="{span:8}"
+        :xl="{span: 10}"
+        :xxl="{span: 12}"
+      >
+        <div>2</div>
+      </g-col>
+      <g-col
+        :xs="{span: 24}"
+        :sm="{span: 24}"
+        :md="{span: 24}"
+        :lg="{span:24}"
+        :xl="{span: 7}"
+        :xxl="{span: 6}"
+      >
+        <div>3</div>
+      </g-col>
+    </g-row>
+    <br>
+    <g-row>
+      <g-col
+        :sm="{span: 4}"
+        :lg="{span:8}"
+        :xxl="{span: 6}"
+      >
+        <div>1</div>
+      </g-col>
+      <g-col
+        :sm="{span: 16}"
+        :lg="{span:8}"
+        :xxl="{span: 12}"
+      >
+        <div>2</div>
+      </g-col>
+      <g-col
+        :sm="{span: 4}"
+        :lg="{span:8}"
+        :xxl="{span: 6}"
+      >
+        <div>3</div>
+      </g-col>
     </g-row>
   </div>
 </template>
@@ -41,10 +146,8 @@
 export default {
   name: 'page-grid',
   data() {
-    return {
-
-    }
-  }
+    return {}
+  },
 }
 </script>
 
